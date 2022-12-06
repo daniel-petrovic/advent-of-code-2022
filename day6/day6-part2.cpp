4a0b03dda3e72036558387fa0993b6dd64f1ffd0
Daniel Petrovic <daniel.petrovic@ebcont.com>
2022-12-06 07:33:36 +0100

    (Day6) Part 2

---
+++ day6/day6-part2.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  getline(cin, s);
+
+  unsigned i = 13;
+  bool is_unique;
+  for (; !is_unique && i < s.size(); ++i) {
+    // i, i-1, i-2, i-3, ..., i-13
+    set<char> x;
+    bool isq = true;
+    for (auto j = i - 13; j <= i; ++j) {
+      isq &= x.insert(s[j]).second;
+    }
+
+    is_unique = isq;
+  }
+
+  cout << i << endl;
+}
