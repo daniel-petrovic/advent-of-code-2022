078945530fd992653a09159485c18603ab86603f
Daniel Petrovic <daniel-dev@hotmail.de>
2022-12-18 13:27:22 +0100

    (Day17) Part 1

--- day17/day17-part1.cpp
+++ day17/day17-part1.cpp
@@ -19,9 +19,9 @@ struct Box {
     int bottom() const noexcept { return y; }
 };
 
-struct Form {
-    Form() = default;
-    Form(Shape s) {
+struct Rock {
+    Rock() = default;
+    Rock(Shape s) {
         shape = std::move(s);
         int xmin = INT_MAX;
         int xmax = 0;
@@ -93,8 +93,8 @@ Shape S5 = {
 };
 
 
-bool can_push_to(int x0, int y0, const Form& f);
-void push(int x0, int y0, Form& f);
+bool can_push_to(int x0, int y0, const Rock& f);
+void push(int x0, int y0, Rock& f);
 
 static constexpr int XMAX = 7; 
 static constexpr int YMAX = 10'000;
@@ -108,74 +108,101 @@ int main()
             occupied[j][i] = false;
         }
 
-    auto forms = array {Form{S1}, Form{S2}, Form{S3}, Form{S4}, Form{S5}};
+    auto rocks = array {Rock{S1}, Rock{S2}, Rock{S3}, Rock{S4}, Rock{S5}};
 
     auto commands = string(istream_iterator<char>{cin}, istream_iterator<char>{});
 
-    Form rock;
+    auto draw = [&] {
+        const auto Y = 20;
+        cout << endl;
+        for (int y = Y; y >= 0; --y) {
+            for (int x = 0; x < XMAX; ++x) {
+                char c = occupied[y][x] ? '#' : '.';
+                cout << c;
+            }
+            cout << endl;
+        }
+        cout << endl;
+    };
 
-    for (int i=0, x0=2, y0=3; i<3; ++i, y0 = rock.box.top() + 4)
+    Rock rock;
+
+    for (int i=0, j=0, x0=2, y0=3; i<2022; ++i, y0 = rock.box.top() + 4)
     {
-        printf("rock %d starts fallinig at (%d, %d)\n", i, x0, y0);
-        rock = forms[i%5];
+        //printf("rock %d starts fallinig at (%d, %d)\n", i, x0, y0);
+        rock = rocks[i%5];
         rock.box.x = x0;
         rock.box.y = y0;
 
-        for (int j=0, x=x0, y=y0;; j = (j+1)%commands.size()) {
+        for (int x=x0, y=y0;;) {
 
-            auto x2 = x + (commands[j] == '>') ? 1 : -1;
-            cout << "\t" << " '" << commands[j] << "' ";
+            if (commands[j] != '>' and commands[j] != '<') {
+                throw "invalid command";
+            }
+
+            auto x2 = x + ((commands[j] == '>') ? 1 : -1);
+            j = (j + 1) % commands.size();
+            //cout << "\t" << " '" << commands[j] << "' ";
 
             if (can_push_to(x2, y, rock)) {
                 push(x2, y, rock);
                 x = x2;
+                //draw();
             }
 
             auto y2 = y-1;
 
             if (can_push_to(x, y2, rock)) {
-                printf("\t pushing rock %d to (x,y2)=(%d,%d)\n", i, x, y2);
                 push(x, y2, rock);
                 y = y2;
+                //draw();
             }
             else {
-                printf("\t: rock %d : cannot push down to y2=%d\n", i, y2);
+                //printf("\t: rock %d : cannot push down to y2=%d\n", i, y2);
                 break;
             }
         }
     } 
 
-    for (int y = 0; y < 14; ++y) {
-        for (int x = 0; x < XMAX; ++x) {
-            char c = occupied[y][x] ? '#' : '.';
-            cout << c;
-        }
-        cout << endl;
-    }
+    //draw();
 
     cout << "height = " << rock.box.top() + 1  << endl;
 }
 
-bool can_push_to(int x0, int y0, const Form& f)
+bool can_push_to(int x0, int y0, const Rock& f)
 {
     if (x0 < 0) return false;
     if (x0 + f.box.w > XMAX) return false;
     if (y0 < 0) return false;
 
+
     for (auto [xp,yp] : f.shape) {
-        auto x = x0 + xp;
-        auto y = y0 + yp;
-        if (occupied[y][x])
+
+        xp += x0;
+        yp += y0;
+
+        bool new_xy = true;
+
+        for (auto [xx, yy] : f.shape) {
+            if (xp == f.box.x + xx and yp == f.box.y + yy) {
+                new_xy = false;
+                break;
+            }
+        }
+
+        if (new_xy and occupied[yp][xp])
             return false;
     }
 
     return  true;
 }
 
-void push(int x0, int y0, Form& f)
+void push(int x0, int y0, Rock& f)
 {
     if (x0 == f.box.x and y0 == f.box.y)
         return;
+
+    //printf("\t pushing rock to (x0,y0)=(%d,%d)\n", x0, y0);
 
     for (auto [xp, yp] : f.shape) {
         auto x = f.box.x + xp;
